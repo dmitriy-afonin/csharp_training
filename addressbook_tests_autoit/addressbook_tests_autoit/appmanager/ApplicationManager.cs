42a1f4d4a15060f7777dece7c85de46642839697
Dmitriy <afonin-d@yandex.ru>
2018-10-04 22:41:26 +0300

    Тесты для добавления и удаления групп реализованы

---
+++ addressbook_tests_autoit/addressbook_tests_autoit/appmanager/ApplicationManager.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoItX3Lib;
+
+
+namespace addressbook_tests_autoit
+{
+
+    public class ApplicationManager
+    {
+        public static string WINTITLE = "Free Address Book";
+        private AutoItX3 aux;
+        private GroupHelper groupHelper;
+        public ApplicationManager()
+        {
+
+            aux = new AutoItX3();
+
+            aux.Run(@"C:\Tests\FreeAddressBookPortable\AddressBook.exe", "" , aux.SW_SHOW);
+
+            aux.WinWait(WINTITLE, "", 500);
+            aux.WinWaitActive(WINTITLE, "", 500);
+
+            groupHelper = new GroupHelper(this);
+        }
+
+        public void Stop()
+        {
+            aux.Sleep(1000);
+            aux.ControlClick(WINTITLE, "&Exit", "WindowsForms10.BUTTON.app.0.3dd72c210");
+
+        }
+
+        public AutoItX3 Aux
+        {
+            get
+            {
+                return aux;
+            }
+        }
+
+        public GroupHelper Groups
+        {
+            get { return groupHelper; }
+        }
+    }
+}
---
+++ addressbook_tests_autoit/addressbook_tests_autoit/appmanager/GroupHelper.cs
@@ -0,0 +1,78 @@
+﻿using System.Text;
+using System.Linq;
+using System.Collections.Generic;
+using System;
+using System.IO;
+
+namespace addressbook_tests_autoit
+{
+    public class GroupHelper : HelperBase
+    {
+        public static string GROUPWINTITLE = "Group editor";
+        public static string DELETEGROUPWINTITLE = "Delete group";
+        public GroupHelper(ApplicationManager manager) : base(manager) { }
+
+        public List<GroupData> GetGroupList()
+        {
+            List<GroupData> list = new List<GroupData>();
+            OpenGroupsDialogue();
+            string count = aux.ControlTreeView(GROUPWINTITLE, "", "WindowsForms10.SysTreeView32.app.0.3dd72c21",
+                "GetItemCount", "#0", "");
+            for (int i = 0; i < int.Parse(count); i++)
+            {
+                string item = aux.ControlTreeView(
+                    GROUPWINTITLE, "", "WindowsForms10.SysTreeView32.app.0.3dd72c21",
+                    "GetText", "#0|#" + i, "");
+                list.Add(new GroupData()
+                {
+                    Name = item
+                });
+            };
+
+            CloseGroupsDialogue();
+
+            return list;
+        }
+
+        public void Add(GroupData newGroup)
+        {
+            OpenGroupsDialogue();
+            aux.ControlClick(GROUPWINTITLE, "", "WindowsForms10.BUTTON.app.0.3dd72c23");
+            aux.Send(newGroup.Name);
+            aux.Send("{ENTER}");
+            CloseGroupsDialogue();
+            return;
+        }
+
+        public void Remove(int groupNumber)
+        {
+
+            OpenGroupsDialogue();
+
+            aux.ControlTreeView(
+                        GROUPWINTITLE, "", "WindowsForms10.SysTreeView32.app.0.3dd72c21",
+                        "Select", "#0|#" + groupNumber, "");
+            aux.Sleep(500);
+            aux.ControlClick(GROUPWINTITLE, "", "WindowsForms10.BUTTON.app.0.3dd72c21");
+
+            aux.WinWait(DELETEGROUPWINTITLE, "", 500);
+            aux.WinWaitActive(DELETEGROUPWINTITLE, "", 500);
+
+            aux.ControlClick(DELETEGROUPWINTITLE, "", "WindowsForms10.BUTTON.app.0.3dd72c23");
+
+            CloseGroupsDialogue();
+            return;
+        }
+
+        private void CloseGroupsDialogue()
+        {
+            aux.ControlClick(GROUPWINTITLE, "", "WindowsForms10.BUTTON.app.0.3dd72c24");
+        }
+
+        private void OpenGroupsDialogue()
+        {
+            aux.ControlClick(WINTITLE, "", "WindowsForms10.BUTTON.app.0.3dd72c212");
+            aux.WinWaitActive(GROUPWINTITLE, "", 500);
+        }
+    }
+}
